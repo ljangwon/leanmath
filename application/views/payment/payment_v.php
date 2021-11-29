d3dbe3c5b7477eeee2ab0bc0fc02faad42984197
jakeleanco <jakeleanco@gmail.com>
2021-11-29 23:52:47 +0900

    init

--- application/views/payment/payment_v.php
+++ application/views/payment/payment_v.php
@@ -22,8 +22,8 @@
     <div class="row">
       <div class="col-12">
         <div class="col-md-12">
-          <h1>Payment
-            <small>List</small>
+          <h1>Home
+            <small>/Payment List</small>
             <?php echo $this->session->flashdata('msg'); ?>
             <div class="form-group float-right">
               <label for="select_month">Month</label>
