98d1f58b4d5b559df48a60a1b76c599b6342d796
jakeleanco <jakeleanco@gmail.com>
2021-12-05 08:39:16 +0900

    leanmath - init

--- application/views/payment/payment_v.php
+++ application/views/payment/payment_v.php
@@ -22,7 +22,7 @@
     <div class="row">
       <div class="col-12">
         <div class="col-md-12">
-          <h1><a style="text-decoration:none color:red" href=" <?php echo base_url() ?>"> LEANMATH </a>
+          <h1><a style="text-decoration:none color:red" href=" <?php echo base_url() ?>"> LEANMATH - new </a>
             <small>/Payment List</small>
             <?php echo $this->session->flashdata('msg'); ?>
             <div class="form-group float-right">
