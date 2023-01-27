a8e4b8368deb6e541d2defc2849fcfa54e0a9315
jakeleanco <jakeleanco@gmail.com>
2023-01-28 01:09:29 +0900

    test

--- application/views/common/s1_head_v.php
+++ application/views/common/s1_head_v.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>leanmath - ERP 2 </title>
+  <title>37080-leanmath </title>
 
   <!-- Custom fonts for this template-->
   <!--
@@ -18,10 +18,10 @@
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="/lmath/admin/css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="/leanmath/admin/css/sb-admin-2.min.css" rel="stylesheet">
 
   <!-- Custom styles for this page -->
-  <link href="/lmath/admin/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+  <link href="/leanmath/admin/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
   <link href="https://code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css" rel="stylesheet">
 
   <style>
