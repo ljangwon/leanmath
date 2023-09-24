19b3b5c942cf58f81a0d9563a28a284f125b00f1
Jake <ljangwon@gmail.com>
2023-09-24 15:59:54 +0900

    2023.09.24

--- application/config/config.php
+++ application/config/config.php
@@ -15,7 +15,7 @@
 |
 */
 //$config['base_url']  = 'http://leanedunas.ipdisk.co.kr:8000/leanmath';
-$config['base_url']  = 'http://leanmath.leanedu22.site';
+$config['base_url']  = 'http://localhost:9090/leanmath';
 //$config['base_url']  = 'http://localhot/leanmath';
 /*
 |--------------------------------------------------------------------------
