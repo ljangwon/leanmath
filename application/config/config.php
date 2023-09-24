662393adaaaabfbb0499f6e3105ebf9dba3a5f51
JakeLee <jakeleanco@gmail.com>
2023-09-24 22:05:03 +0900

    2023_0924

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-//$config['base_url']  = 'http://leanedunas.ipdisk.co.kr:8000/leanmath';
+//$config['base_url']  = 'http://leanedu22.site:8000/leanmath';
 //$config['base_url']  = 'http://leanmath.leanedu22.site';
 $config['base_url']  = 'http://localhost:9090/leanmath';
 /*
